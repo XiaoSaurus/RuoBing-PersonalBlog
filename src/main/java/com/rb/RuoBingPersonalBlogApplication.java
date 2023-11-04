8b0d554077644ca4ae9be309983c31ccfbd23d5c
XiaoSaurus_dev <464226879@qq.com>
2023-11-04 11:46:19 +0800

    Initial commit

---
+++ src/main/java/com/rb/RuoBingPersonalBlogApplication.java
@@ -0,0 +1,13 @@
+package com.rb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuoBingPersonalBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RuoBingPersonalBlogApplication.class, args);
+    }
+
+}
